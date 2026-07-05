1bc4a9083fa67b93370e7ba82029c8a2a9a2485c
Puneet Chugh <puneetchugh@Puneets-MacBook-Pro.local>
2026-07-05 13:38:10 -0700

    DP: Longest Common Subsequence

--- app/src/main/java/com/example/interviewprepration/ds_n_algo/DynamicProgramming.kt
+++ app/src/main/java/com/example/interviewprepration/ds_n_algo/DynamicProgramming.kt
@@ -1,6 +1,7 @@
 package com.example.interviewprepration.ds_n_algo
 
 import android.util.Log
+import kotlin.math.max
 import kotlin.math.min
 
 object DynamicProgramming {
@@ -10,6 +11,16 @@ object DynamicProgramming {
     fun driverFunction() {
         val minCoins = coinChange(coinList = intArrayOf(1, 3, 5), target = 11)
         Log.e(LOG_TAG, "Inside driverFunction, coinChange: $minCoins")
+
+        val text11 = "ABC"
+        val text12 = "ACD"
+        val lcs1 = longestCommonSubsequence(text11, text12)
+        Log.e(LOG_TAG, "Inside driverFunction, lcs1 for $text11 and $text12: $lcs1")
+
+        val text21 = "AGGTAB"
+        val text22 = "GXTXAYB"
+        val lcs2 = longestCommonSubsequence(text21, text22)
+        Log.e(LOG_TAG, "Inside driverFunction, lcs1 for $text21 and $text22: $lcs2")
     }
 
     // min number of coins to make a target sum
@@ -32,5 +43,34 @@ object DynamicProgramming {
         } else {
             outputArray[target]
         }
+    }
+
+    fun longestCommonSubsequence(text1: String, text2: String): Int {
+
+        val dp = Array(text1.length + 1) { IntArray(text2.length + 1) { 0 } }
+
+        dp[0][0] = 0
+
+        for (input1Counter in 1..text1.length) {
+            for (input2Counter in 1..text2.length) {
+                if (text1[input1Counter - 1] == text2[input2Counter - 1]) {
+                    Log.e(
+                        LOG_TAG,
+                        "Inside lcs(), input1Counter: $input1Counter, input2Counter: $input2Counter, match found: ${text1[input1Counter - 1]}"
+                    )
+                    dp[input1Counter][input2Counter] = dp[input1Counter - 1][input2Counter - 1] + 1
+                } else {
+                    dp[input1Counter][input2Counter] = max(
+                        dp[input1Counter - 1][input2Counter],
+                        dp[input1Counter][input2Counter - 1]
+                    )
+                }
+            }
+        }
+        Log.e(LOG_TAG, "Inside lcs(), dp")
+        dp.forEach {
+            Log.e(LOG_TAG, "Inside lcs(), dp: ${it.contentToString()}")
+        }
+        return dp[text1.length][text2.length]
     }
 }
