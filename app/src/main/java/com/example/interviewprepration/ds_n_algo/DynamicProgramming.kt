3ed012095327f1085ed64d58efea7fb311d32586
Puneet Chugh <puneetchugh@Puneets-MacBook-Pro.local>
2026-08-12 04:42:03 -0700

    Knapsack by recursion

--- app/src/main/java/com/example/interviewprepration/ds_n_algo/DynamicProgramming.kt
+++ app/src/main/java/com/example/interviewprepration/ds_n_algo/DynamicProgramming.kt
@@ -21,6 +21,8 @@ object DynamicProgramming {
         val text22 = "GXTXAYB"
         val lcs2 = longestCommonSubsequence(text21, text22)
         Log.e(LOG_TAG, "Inside driverFunction, lcs1 for $text21 and $text22: $lcs2")
+
+        knapSack()
     }
 
     // min number of coins to make a target sum
@@ -72,5 +74,49 @@ object DynamicProgramming {
             Log.e(LOG_TAG, "Inside lcs(), dp: ${it.contentToString()}")
         }
         return dp[text1.length][text2.length]
+    }
+
+    fun knapSack() {
+        val weights = intArrayOf(1, 2, 3, 8, 7, 4)
+        val values = intArrayOf(20, 5, 10, 40, 15, 25)
+
+        Log.e(
+            LOG_TAG, "Inside knapSack(), max profit: ${
+                knapSackHelper(
+                    values = values,
+                    weights = weights,
+                    remaining = 10,
+                    index = 0,
+                    includedPairs = emptyList()
+                )
+            }"
+        )
+    }
+
+    fun knapSackHelper(values: IntArray, weights: IntArray, remaining: Int, index: Int, includedPairs: List<Pair<Int, Int>>): Int {
+        if (remaining < 0) {
+            return Int.MIN_VALUE
+        }
+        if (index >= values.size || remaining == 0) {
+            Log.e(LOG_TAG, "Inside knapSackHelper(), total value: ${includedPairs.sumOf { it.first }}, includedPairs: ${includedPairs.joinToString()}")
+            return 0
+        }
+
+        val including = values[index] + knapSackHelper(
+            values = values,
+            weights = weights,
+            remaining = remaining - weights[index],
+            index = index + 1,
+            includedPairs = includedPairs + Pair(values[index], weights[index])
+        )
+
+        val excluding = knapSackHelper(
+            values = values,
+            weights = weights,
+            remaining = remaining,
+            index = index + 1,
+            includedPairs = includedPairs
+        )
+        return max(including, excluding)
     }
 }
